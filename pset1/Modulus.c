6774fdfd30d1e029dce46d9052272b838f74271e
MarcoF1 <marco1fleming@gmail.com>
2021-01-12 17:02:01 -0600

    added a test case to be read from .txt file

--- pset1/Modulus.c
+++ pset1/Modulus.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 
 int main() {
+
     int N, m, M;
     scanf("%d %d %d", &N, &m, &M);  
-    printf("%d %d %d", N, m, M);
+    printf("%d %d %d\n", N, m, M);
+
+    int num;
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &num);  
+        printf("%d\n", num);
+    }
+
     return 0;
 }
--- pset1/test.sh
+++ pset1/test.sh
@@ -3,8 +3,13 @@
 #compile the C file
 gcc -o Mod.out Modulus.c
 
+# file to be reading input lines from
+filename='ModulusArgs.txt'
+
 # run the file with inputs
-./Mod.out
+./Mod.out cat<< EOF
+$(cat $filename)
+EOF
 
 # Clean the output files
 rm Mod.out
