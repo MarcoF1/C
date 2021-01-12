30d9d728bc6433149c5696e7d7f756fc93800343
MarcoF1 <marco1fleming@gmail.com>
2021-01-12 17:15:44 -0600

    finished problem 1

--- pset1/problem 1/Modulus.c
+++ pset1/problem 1/Modulus.c
@@ -1,16 +1,25 @@
 #include <stdio.h>
+// #include <stdlib.h>
 
 int main() {
 
     int N, m, M;
+    // scan N, m, and M
     scanf("%d %d %d", &N, &m, &M);  
-    printf("%d %d %d\n", N, m, M);
 
+    int congruentCount = 0;
     int num;
+    int remainder;
+    // count how many inputs ar congruent to m mod M
     for(int i = 0; i < N; i++) {
         scanf("%d", &num);  
-        printf("%d\n", num);
+        remainder = m-num % M;
+
+        if (remainder == 0) {
+            congruentCount++;
+        }
     }
 
+    printf("%d", congruentCount);
     return 0;
 }
